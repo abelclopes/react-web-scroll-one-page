d53c12db5e66098fb8ff3c8b2c26b7de6d981a8d
Abel Lopes <abel.lopes@softdesign.com.br>
2020-05-13 22:17:00 -0300

    run on fullpage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import ReactFullpage from '@fullpage/react-fullpage';
+import NavigateLinks from './navigateScroll/index';
+class MySection extends React.Component {
+  render() {
+    return (
+      <div className="section">
+        <h3>{this.props.content}</h3>
+      </div>
+    );
+  }
+}
+const  App = (props) => {
+ 
+const anchors = ["home", "page2", "page3", "page4", "page5"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavigateLinks/>
+      <ReactFullpage
+        anchors={anchors}
+        scrollingSpeed = {1000} /* Options here */
+
+        render={({ state, fullpageApi }) => {
+          console.log("render prop change", state, fullpageApi);
+          return (
+            <ReactFullpage.Wrapper>
+              
+              <MySection content={'Section 1'}/>
+              <MySection content={'Section 2'}/>
+              <MySection content={'Section 3'}/>
+              <MySection content={'Section 4'}/>
+              <MySection content={'Section 5'}/>  
+              
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
     </div>
-  );
+    )
 }
 
 export default App;
